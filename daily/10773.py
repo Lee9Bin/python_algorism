6766e4851f1ea3e625b97b835daaeb26a0b8e5b6
Lee9Bin <imagen33@daum.net>
2023-05-29 21:42:03 +0900

    commit message

--- daily/10773.py
+++ daily/10773.py
@@ -3,9 +3,23 @@ import sys
 n = int(sys.stdin.readline())
 stack = []
 for i in range(n):
-    x = int(sys.stdin.readline())
-    if x != 0:
-        stack.append(x)
-    elif x == 0:
-        stack.pop()
-print(stack)
+    x = list(map(str,sys.stdin.readline().split()))
+    if x[0] == 'push':
+        stack.append(x[1])
+    elif x[0] == 'top':
+        if len(stack) >0:
+            print(stack[-1])
+        else:
+            print(-1)
+    elif x[0] == 'pop':
+        if len(stack) >0:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif x[0] == 'size':
+        print(len(stack))
+    elif x[0] == 'empty':
+        if len(stack) >0:
+            print(0)
+        else:
+            print(1)
