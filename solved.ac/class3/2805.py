971d8d04f5bed109b90aa1253fd52ffaecb58981
Lee9Bin <imagen33@daum.net>
2023-06-27 17:24:12 +0900

    [백준] 2805번

---
+++ solved.ac/class3/2805.py
@@ -0,0 +1,25 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+tree = list(map(int, sys.stdin.readline().split()))
+tree.sort()
+
+start = tree[0]
+end = tree[-1]
+ans = 0
+while start<=end:
+    result = 0
+    mid = (start+end)//2
+    for i in tree:
+        if i > mid:
+            result += i-mid
+    if result > m:
+        start = mid+1
+        if ans < mid:
+            ans = mid
+    elif result < m:
+        end = mid-1
+    else:
+        ans = mid
+        break
+print(ans)
