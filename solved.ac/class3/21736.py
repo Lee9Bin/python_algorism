0ff15fac7d5933c4629d06a6f0a9257ece3862d0
Lee9Bin <imagen33@daum.net>
2023-06-29 17:55:54 +0900

    [백준] 21736번

--- solved.ac/class3/21736.py
+++ solved.ac/class3/21736.py
@@ -19,6 +19,7 @@ def dfs(x,y):
     if x<0 or y<0 or x>n-1 or y>m-1:
         return
     
+    # I,O,P면 탐색 가능
     if graph[x][y] == 'I' or graph[x][y] == 'O' or graph[x][y] == 'P':
         if graph[x][y] == 'P':
             cnt +=1
@@ -27,6 +28,7 @@ def dfs(x,y):
         dfs(x,y+1)
         dfs(x-1,y)
         dfs(x,y-1)
+        
 cnt = 0        
 dfs(nowx,nowy)
 if cnt == 0:
