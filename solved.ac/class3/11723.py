60a8a956a691e2803cefef05e4fd5280eecb9357
Lee9Bin <imagen33@daum.net>
2023-08-15 19:45:57 +0900

    [백준] 11723번 재채점

--- solved.ac/class3/11723.py
+++ solved.ac/class3/11723.py
@@ -1,30 +1,32 @@
 import sys
 m = int(sys.stdin.readline())
-s = []
-All = []
+s = {}
 for i in range(1,21):
-    All.append(i)
-emty = []
+    s[str(i)] = 0
 
 for i in range(m):
-    command = list(map(str, sys.stdin.readline().split()))
-    if command[0] == "add":
-        if int(command[1]) not in s:
-            s.append(int(command[1]))
-    if command[0] == "remove":
-        if int(command[1]) in s:
-            s.remove(int(command[1]))
-    if command[0] == "check":
-        if int(command[1]) in s:
+    try:
+        commend, num = map(str,sys.stdin.readline().split())
+    except:
+        if commend == "all":
+            for i in range(1,21):
+                s[str(i)] = 1
+        if commend == "empty":
+            for i in range(1,21):
+                s[str(i)] = 0
+    if commend == "add":
+        if s[num] == 0:
+            s[num] = 1
+    if commend == "remove":
+        if s[num] == 1:
+            s[num] = 0
+    if commend == "check":
+        if s[num] == 1:
             print(1)
         else:
             print(0)
-    if command[0] == "toggle":
-        if int(command[1]) in s:
-            s.remove(int(command[1]))
+    if commend == "toggle":
+        if s[num] == 1:
+            s[num] = 0
         else:
-            s.append(int(command[1]))
-    if command[0] == "all":
-        s = All
-    if command[0] == "empty":
-        s = emty
+            s[num] = 1
