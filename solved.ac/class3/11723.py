2a8f9fb48e864b012c50cbae0c9b558cae371c43
Lee9Bin <imagen33@daum.net>
2023-08-15 19:49:26 +0900

    [백준] 11723번 재채점

--- solved.ac/class3/11723.py
+++ solved.ac/class3/11723.py
@@ -1,32 +1,26 @@
 import sys
 m = int(sys.stdin.readline())
-s = {}
-for i in range(1,21):
-    s[str(i)] = 0
+s = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0}
 
 for i in range(m):
-    try:
-        commend, num = map(str,sys.stdin.readline().split())
-    except:
-        if commend == "all":
-            for i in range(1,21):
-                s[str(i)] = 1
-        if commend == "empty":
-            for i in range(1,21):
-                s[str(i)] = 0
-    if commend == "add":
-        if s[num] == 0:
-            s[num] = 1
-    if commend == "remove":
-        if s[num] == 1:
-            s[num] = 0
-    if commend == "check":
-        if s[num] == 1:
+    commend= list(map(str,sys.stdin.readline().split()))
+    if commend[0] == "all":
+        s = {'1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1, '7': 1, '8': 1, '9': 1, '10': 1, '11': 1, '12': 1, '13': 1, '14': 1, '15': 1, '16': 1, '17': 1, '18': 1, '19': 1, '20': 1}
+    if commend[0] == "empty":
+        s = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0}
+    if commend[0] == "add":
+        if s[commend[1]] == 0:
+            s[commend[1]] = 1
+    if commend[0] == "remove":
+        if s[commend[1]] == 1:
+            s[commend[1]] = 0
+    if commend[0] == "check":
+        if s[commend[1]] == 1:
             print(1)
         else:
             print(0)
-    if commend == "toggle":
-        if s[num] == 1:
-            s[num] = 0
+    if commend[0] == "toggle":
+        if s[commend[1]] == 1:
+            s[commend[1]] = 0
         else:
-            s[num] = 1
+            s[commend[1]] = 1
