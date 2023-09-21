0549fecb7dc73e8a0281ed8099b213b96e6a6c6a
Lee9Bin <imagen33@daum.net>
2023-09-21 17:59:10 +0900

    [백준] 20040번

---
+++ solved.ac/class5/20040.py
@@ -0,0 +1,32 @@
+import sys
+n, m = map(int,sys.stdin.readline().split())
+parent = [0] * n
+
+for i in range(n):
+    parent[i] = i
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a,b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+cycle = False
+result = 0
+for i in range(m):
+    a, b = map(int,sys.stdin.readline().split())
+    if cycle == False and parent[a] == parent[b]:
+        result = i+1
+        cycle = True
+    union_parent(parent,a,b)
+
+if cycle == False:
+    print(0)
+else:
+    print(result)
