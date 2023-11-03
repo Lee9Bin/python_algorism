ac4c26fedcf1dfbff97b17d645d4c17c5101e0c5
Lee9Bin <imagen33@daum.net>
2023-11-03 22:22:38 +0900

    [SWEA] 1946번

--- swea/d2/1946.py
+++ swea/d2/1946.py
@@ -1,16 +1,14 @@
 t = int(input())
-grade = ["A+", "A0", "A-","B+", "B0", "B-","C+", "C0", "C-","D0"]
 
 for tc in range(1,t+1):
-    n, k =map(int,input().split())
-    student = []
-    
+    n = int(input())
+    allString = ""
+    length = 0
     for i in range(n):
-        mid, end, homework = map(int,input().split())
-        student.append(mid*0.35 + end*0.45 + homework*0.2)
-    findScore = student[k-1]
-    
-    student.sort(reverse=True)
-
-    change = n//10
-    print(f"#{tc} {grade[student.index(findScore)//change]}")
+        word = list(map(str,input().split()))
+        allString += word[0]*int(word[1])
+        length += int(word[1])
+        
+    print(f"#{tc}")
+    for i in range(length//10+1):
+        print(allString[i*10:i*10+9])
