9176c261ba5356292de0df54b32614ad773e53ed
Lee9Bin <imagen33@daum.net>
2023-11-09 10:48:10 +0900

    [백준] 10971번

--- solved.ac/breaktime/10971.py
+++ solved.ac/breaktime/10971.py
@@ -6,6 +6,18 @@ for i in range(n):
     graph.append(list(map(int,sys.stdin.readline().split())))
 
 ans = 1e9
+def back(start,v,root):
+    global ans
+    if visited.count(True) == n-1:
+        if graph[start][root] != 0:
+            ans = min(ans,v+graph[start][root])
+        return
+    for i in range(n):
+        if graph[start][i] != 0 and visited[i] == False and i != root:
+            visited[i] = True
+            back(i,v+graph[start][i],root)
+            visited[i] = False
+
 def back(start,v,cnt):
     global ans
     if v > ans:
