64c4039406db77b9f7e7f7b2c5213c0202f103e3
Lee9Bin <imagen33@daum.net>
2023-11-12 22:12:26 +0900

    [프로그래머스] 다리를 지나는 트럭

--- programmers/level2/다리를_지나는_트럭.py
+++ programmers/level2/다리를_지나는_트럭.py
@@ -1,3 +1,11 @@
+# 문제설명: 다리가 있을 때 트럭이 다 지나가기 위해 걸리는 시간을 구하기
+#         다리의 길이가 있고 다리가 버틸 수 있는 무게가 제한 돼 있다.
+# 생각한 풀이
+# 문제에 주어진 다리의 길이만큼 0인 큐로 만든다.
+# 트럭배열에서 맨앞에 트럭 무게를 확인하고 현재 다리가 버틸수 있는 무게면 빼서 다리에 넣어주고 아니면 0으로 채운다
+# 다리 배열 앞에서 빼고 뒤에서 넣어주고 ! -> 큐가 유리하다.
+# 앞에서 뺏을때 0이 아닌 숫자면 트럭이니깐 카운팅 -> 반복문의 종료 조건이 될것
+# 현재 다리 위애 있는 것들의 무게 저장할 변수 선언
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
@@ -19,10 +27,8 @@ def solution(bridge_length, weight, truck_weights):
             if que[0] + nowWeight <= weight:
                 nowWeight += que[0]
                 bridge.append(que.popleft())
-            else:
-                bridge.append(0)
-        else:
-            bridge.append(0)
+                continue
+        bridge.append(0)
 
     return answer
 
