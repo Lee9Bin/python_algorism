234cf35ece03b76630078747a8d48972734b18a5
Lee9Bin <imagen33@daum.net>
2023-11-19 18:51:07 +0900

    [백준] 11722번

--- solved.ac/breaktime/11722.py
+++ solved.ac/breaktime/11722.py
@@ -3,12 +3,12 @@ import sys
 n = int(sys.stdin.readline())
 
 numlist = list(map(int, sys.stdin.readline().split()))
-numlist.reverse()
-print(numlist)
+
 dp = [1] * n
 
-for i in range(1,n):
-    for j in range(i, n):
-        if numlist[j] < numlist[i]:
-            dp[j] = max(dp[i]+1, dp[j])
+for i in range(1, n):
+    for j in range(i):
+        if numlist[j] > numlist[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
 print(max(dp))
