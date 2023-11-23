20f5ab5a20812684e6dff44963b5fc647130e7a3
Lee9Bin <imagen33@daum.net>
2023-11-23 10:03:42 +0900

    [백준] 11728번

--- solved.ac/breaktime/11728.py
+++ solved.ac/breaktime/11728.py
@@ -2,11 +2,33 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 result = []
 
+# alist = list(map(int, sys.stdin.readline().split()))
+# blist = list(map(int, sys.stdin.readline().split()))
+
+# result = alist + blist
+
+# result.sort()
+
+# print(*result)
+
 alist = list(map(int, sys.stdin.readline().split()))
 blist = list(map(int, sys.stdin.readline().split()))
 
-result = alist + blist
+alist.sort()
+blist.sort()
+aStart = 0
+bStart = 0
 
-result.sort()
+while aStart<n and bStart <m:
+    if alist[aStart] <= blist[bStart]:
+        result.append(alist[aStart])
+        aStart +=1
+    else:
+        result.append(blist[bStart])
+        bStart +=1
+for i in range(aStart,n):
+    result.append(alist[i])
+for i in range(bStart,m):
+    result.append(blist[i])
 
 print(*result)
