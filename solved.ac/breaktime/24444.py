64903aa1a4bca12667d0dd9845dd685d83a1c439
Lee9Bin <imagen33@daum.net>
2023-11-24 20:05:20 +0900

    [백준] 24444번

---
+++ solved.ac/breaktime/24444.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+n, m ,r = map(int, sys.stdin.readline().split())
+visited = [0] * (n+1)
+visited[r] = 1
+graph = [[] for i in range(n+1)]
+
+for i in range(m):
+    a,b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1,n+1):
+    graph[i].sort()
+    
+que = deque()
+que.append(r)
+cnt = 1
+
+while que:
+    nowV = que.popleft()
+    
+    for i in graph[nowV]:
+        if visited[i] == 0:
+            cnt+=1
+            visited[i] = cnt
+            que.append(i)
+
+for i in visited[1:]:
+    print(i)
