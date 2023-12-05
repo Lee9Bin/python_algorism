09d83dec215a37ff518219fd73e32e3c9ec90a50
Lee9Bin <imagen33@daum.net>
2023-12-06 00:56:48 +0900

    [백준] 5347번

---
+++ solved.ac/breaktime/5347.py
@@ -0,0 +1,12 @@
+import sys
+# 일단 공차 K랑 차이가 가장 작은것은 찾자
+def GCD(x, y):
+    while y > 0:
+        x, y = y, x % y
+    return x
+
+t = int(sys.stdin.readline())
+for i in range(t):
+    a, b = map(int, sys.stdin.readline().split())
+
+    print(a*b//GCD(a,b))
