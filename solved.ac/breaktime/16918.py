b14f4460a1fdd6ccae4c6367289dd2e30a2033fc
Lee9Bin <imagen33@daum.net>
2024-01-24 20:16:31 +0900

    [백준] 16918번

--- solved.ac/breaktime/16918.py
+++ solved.ac/breaktime/16918.py
@@ -8,38 +8,29 @@ for i in range(r):
 
 time = 0
 boomb = deque()
-for i in range(r):
-    for j in range(c):
-        if graph[i][j] == "O":
-            boomb.append((2,(i,j)))
 
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
 while time < n:
-    if time == 0:
-        time += 1
-        continue
-    elif time % 2 != 0:
-        for i in range(r):
-            for j in range(c):
-                if graph[i][j] == ".":
-                    graph[i][j] = "O"
-                    boomb.append((time+3,(i,j)))
-    elif time % 2 == 0:
-        while True:
+    if time % 2 == 0:
+        while boomb:
             nowTime, xy = boomb.popleft()
-            if nowTime != time:
-                boomb.appendleft((nowTime, (xy[0],xy[1])))
-                break
             graph[xy[0]][xy[1]] = "."
             for i in range(4):
                 nextx = xy[0] + dx[i]
                 nexty = xy[1] + dy[i]
                 if 0 <= nextx < r and 0 <= nexty < c:
                     graph[nextx][nexty] = "."
-                    if (nowTime+2,(nextx,nexty)) in boomb:
-                        boomb.remove((nowTime+2,(nextx,nexty)))
+        for i in range(r):
+            for j in range(c):
+                if graph[i][j] == "O":
+                    boomb.append((time+2,(i,j)))
+    elif time % 2 != 0:
+        for i in range(r):
+            for j in range(c):
+                if graph[i][j] == ".":
+                    graph[i][j] = "O"
     time += 1
 for i in graph:
     print(''.join(i))
